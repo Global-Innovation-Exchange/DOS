a58b25bb99dee6cbf294e9715f44d3735b57f712
Steven Guh <stevenguh@gmail.com>
2020-05-03 19:37:53 -0700

    Add clear and save button

--- lib/detail.dart
+++ lib/detail.dart
@@ -4,15 +4,31 @@ class DetailWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Detail')),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+        appBar: AppBar(
+          title: Text('Detail'),
+          leading: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          actions: <Widget>[
+            FlatButton(
+              textColor: Colors.white,
+              child: Text('SAVE'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
         ),
-      )
-    );
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go back!'),
+          ),
+        ));
   }
 }
