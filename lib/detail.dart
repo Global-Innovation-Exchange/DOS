a87a6e59cb635027c92dbf311ad760945fecbfac
AmalTurtle <amal91@uw.edu>
2020-05-21 22:15:38 -0700

    - Edit Scale implementation - Edit date/ time implementation

--- lib/detail.dart
+++ lib/detail.dart
@@ -19,23 +19,7 @@ class EmotionDetail extends StatelessWidget {
       padding: EdgeInsets.all(8.0),
 
       // margin: EdgeInsets.only(top: 4),
-      child: TextFormField(
-          initialValue: formatDateTime(log.dateTime),
-          readOnly: true,
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            fillColor: Colors.white,
-            filled: true,
-            prefixIcon: Icon(Icons.calendar_today),
-            prefixText: "Log Date",
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          )),
+      child: DateTimeChange(log: log),
     );
 
     Widget selectedEmotion = Container(
@@ -45,15 +29,8 @@ class EmotionDetail extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           getEmotionImage(log.emotion),
-          Slider(
-            value: log.scale.toDouble(),
-            min: 1.0,
-            max: 5.0,
-            activeColor: Color(0xffE1B699),
-            inactiveColor: Colors.black12,
-            divisions: 4,
-            label: log.scale.toString(),
-            onChanged: null,
+          Container(
+            child: ScaleChange(log: log),
           ),
         ],
       ),
@@ -173,6 +150,133 @@ class EmotionDetail extends StatelessWidget {
 
 Widget _createChip(String value) {
   return Chip(avatar: CircleAvatar(child: Text('#')), label: Text(value));
+}
+
+class ScaleChange extends StatefulWidget {
+  ScaleChange({Key key, this.log}) : super(key: key);
+  final EmotionLog log;
+
+  @override
+  _ScaleChangeState createState() => _ScaleChangeState(log);
+}
+
+class _ScaleChangeState extends State<ScaleChange> {
+  EmotionLog _log;
+  //TextEditingController _jorunalController;
+
+  _ScaleChangeState(EmotionLog log) {
+    this._log = log;
+    //this._jorunalController =
+    //TextEditingController(text: _log.scale.toString());
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    // _jorunalController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _log.scale.toDouble(),
+      min: 1.0,
+      max: 5.0,
+      activeColor: Color(0xffE1B699),
+      inactiveColor: Colors.black12,
+      divisions: 4,
+      label: _log.scale.toString(),
+
+      //controller: _jorunalController,
+      onChanged: (value) {
+        // Not wrapping in setState because field is manged by controller
+        _log.scale = value.toInt();
+        setState(() {
+          _log.scale = value.toInt();
+        });
+      },
+    );
+  }
+}
+
+class DateTimeChange extends StatefulWidget {
+  DateTimeChange({Key key, this.log}) : super(key: key);
+  final EmotionLog log;
+
+  @override
+  _DateTimeChangeState createState() => _DateTimeChangeState(log);
+}
+
+class _DateTimeChangeState extends State<DateTimeChange> {
+  _DateTimeChangeState(EmotionLog log) {
+    this._log = log;
+    this._dateTimeController =
+        TextEditingController(text: formatDateTime(_log.dateTime));
+  }
+
+  EmotionLog _log;
+  TextEditingController _dateTimeController;
+
+  Future _selectDateTime(BuildContext context) async {
+    final DateTime selectedDate = await showDatePicker(
+      context: context,
+      initialDate: _log.dateTime,
+      firstDate: DateTime(_log.dateTime.year - 10),
+      lastDate: DateTime.now(),
+    );
+    if (selectedDate == null) return;
+
+    final TimeOfDay selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(_log.dateTime),
+    );
+    if (selectedTime == null) return;
+
+    final newDateTime = DateTime(
+      selectedDate.year,
+      selectedDate.month,
+      selectedDate.day,
+      selectedTime.hour,
+      selectedTime.minute,
+    );
+    setState(() {
+      _log.dateTime = newDateTime;
+      _dateTimeController.text = formatDateTime(newDateTime);
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _dateTimeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _dateTimeController,
+      readOnly: true,
+      textAlign: TextAlign.center,
+      decoration: InputDecoration(
+        fillColor: Colors.white,
+        filled: true,
+        prefixIcon: Icon(Icons.calendar_today),
+        prefixText: "Enter Date",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+      onTap: () {
+        _selectDateTime(context);
+      },
+    );
+  }
 }
 
 class JournalChange extends StatefulWidget {
